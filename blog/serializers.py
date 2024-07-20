1bc2ddbd81f1a2a5ae4fb59f943af612ac4d3234
Guilherme Amaral <guilherme.amaral@metasix.com.br>
2024-07-19 22:11:55 -0300

    Removing author from the post model

--- blog/serializers.py
+++ blog/serializers.py
@@ -4,11 +4,11 @@ from .models import Post, Comment
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
-        fields = '__all__'
+        fields = ['id', 'name', 'email', 'content', 'created_at', 'updated_at']
 
 class PostSerializer(serializers.ModelSerializer):
     comments = CommentSerializer(many=True, read_only=True)
 
     class Meta:
         model = Post
-        fields = ['id', 'title', 'content', 'author', 'created_at', 'updated_at', 'comments']
+        fields = ['id', 'title', 'content', 'created_at', 'updated_at', 'comments']
